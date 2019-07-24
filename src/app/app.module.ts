a26b538d3ebc39497bb9f5f2e8fbad9e06efb7c7
Prince Raj <princeraj427@gmai.com>
2019-07-24 13:39:25 +0530

    Not Found component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PostFormComponent } from './components/post-form/post-form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { PostComponent } from './components/post/post.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 
@@ -28,7 +29,8 @@ import { PostComponent } from './components/post/post.component';
     PostsComponent,
     PostFormComponent,
     HomeComponent,
-    PostComponent
+    PostComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
